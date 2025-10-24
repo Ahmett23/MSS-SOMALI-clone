36eb57b61749c86813b4db9a26312fe65de6e01e
Ahmett_updi <iamaxmedd@gmail.com>
2025-10-24 21:34:05 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
 
-import Competitors from './Components/competitors/competitors'
-import Header from './Components/header/Header'
-import Time from './Components/timer/Time'
-import VoteModal from './Components/modal/voteModal'
+// import Competitors from './Components/competitors/competitors'
+// import Header from './Components/header/Header'
+// import Time from './Components/timer/Time'
+// import VoteModal from './Components/modal/voteModal'
+// import Timer from './Components/timer/timer'
+import Competitors from './components/competitors/Competitors'
+import Header from './components/header/Header'
+// import Time from './components/timer/Time'
+import VoteModal from './components/modal/VoteModal'
 import Timer from './Components/timer/timer'
+
+
 
 
 function App() {
